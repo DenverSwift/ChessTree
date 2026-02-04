ff76eb91f4df32e9dbf7532eb79d6c48dedcced7
BrayanMist <brayanmist.ua@gmail.com>
2026-02-04 18:20:28 +0200

    /
    Signed-off-by: BrayanMist <brayanmist.ua@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+loadTreesToList();
+
 function log() {
     alert("The button is clicked.");
 }
@@ -44,8 +46,84 @@ function returnButton() {
 
 // JavaScript - логика
 const dialog = document.getElementById('nameDialog');
+const form = dialog.querySelector('form');
 const openBtn = document.getElementById('openBtn');
 const cancelBtn = document.getElementById('cancelBtn');
 
 openBtn.addEventListener('click', () => dialog.showModal());
 cancelBtn.addEventListener('click', () => dialog.close());
+
+form.addEventListener('submit', (e) => {
+    const formData = new FormData(form);
+    const treeName = formData.get('name');
+
+    if (treeName) {
+        createEmptyTree(treeName);
+
+        loadTreesToList();
+
+        form.reset();
+    }
+});
+
+function addNewTree(name) {
+    const select = document.querySelector('.select-list');
+
+    const values = Array.from(select.options).map(opt => parseInt(opt.value) || 0);
+
+    const maxValue = values.length > 0 ? Math.max(...values) : 0;
+    const newValue = maxValue + 1;
+
+    const newOption = document.createElement('option');
+    newOption.value = newValue;
+    newOption.textContent = name;
+
+    select.appendChild(newOption);
+    select.value = newValue;
+}
+
+function generateId() {
+    return Number(getTime().replace(/\D/g, '')).toString(36);
+}
+
+function getTime() {
+    return new Date()
+        .toISOString()
+        .replace(/[ZT]/g, (m) => (m === 'T' ? ' ' : ''));
+}
+
+function createEmptyTree(name) {
+    let jsonPattern = {
+        meta: {
+            name: name,
+            createdAt: getTime(),
+            updatedAt: getTime(),
+        },
+        nodes: [],
+    };
+    localStorage.setItem(`./${name}.json`, JSON.stringify(jsonPattern, null, 2));
+    return 0;
+}
+
+function loadTreesToList() {
+    const select = document.querySelector('.select-list');
+    select.innerHTML = '';
+
+    let maxId = 0;
+
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+
+        if (key.startsWith('./') && key.endsWith('.json')) {
+            const displayName = key.replace('./', '').replace('.json', '');
+
+            const newOption = document.createElement('option');
+
+            maxId++;
+            newOption.value = maxId;
+            newOption.textContent = displayName;
+
+            select.appendChild(newOption);
+        }
+    }
+}
